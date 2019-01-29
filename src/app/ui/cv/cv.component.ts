142264580f2c07962fb38fd18f0b1fb570304104
knksknsy <k.keskinsoy@gmail.com>
2019-01-30 00:05:03 +0100

    Enhance print quality of CV.

--- src/app/ui/cv/cv.component.ts
+++ src/app/ui/cv/cv.component.ts
@@ -25,14 +25,14 @@ export class CVComponent implements OnInit {
     contactAltElement.className = contactAltElement.className.replace(' hide', '');
 
     html2canvas(data[0]).then(canvas => {
-      const contentDataURL = canvas.toDataURL('image/jpeg', 1.0);
+      const contentDataURL = canvas.toDataURL('image/svg+xml', 1.0);
       let pdf = new jspdf('p', 'mm', 'a4');
       pdf.addImage(contentDataURL, 'JPEG', 0, 0, 210, 297);
       return pdf;
     })
       .then(pdf => {
         html2canvas(data[1]).then(canvas => {
-          const contentDataURL = canvas.toDataURL('image/jpeg', 1.0);
+          const contentDataURL = canvas.toDataURL('image/svg+xml', 1.0);
           pdf.addPage('a4', 'p');
           pdf.addImage(contentDataURL, 'JPEG', 0, 0, 210, 297);
           pdf.save('Lebenslauf_Kaan_Keskinsoy.pdf');
