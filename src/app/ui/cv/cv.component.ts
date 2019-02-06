b1c8b696aec0e439286cf58046aab49805309ccc
knksknsy <k.keskinsoy@gmail.com>
2019-02-06 15:49:52 +0100

    Fix CV download bug.

--- src/app/ui/cv/cv.component.ts
+++ src/app/ui/cv/cv.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-cv',
@@ -7,7 +8,7 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
   styleUrls: ['./cv.component.scss']
 })
 export class CVComponent implements OnInit {
-  
+
   public isGerman: boolean;
   public fileGerman = 'Lebenslauf_Kaan_Keskinsoy';
   public fileEnglish = 'CV_Kaan_Keskinsoy';
@@ -15,14 +16,19 @@ export class CVComponent implements OnInit {
   constructor(public translate: TranslateService) { }
 
   ngOnInit() {
+    this.setLanguage(this.translate.currentLang);
     this.translate.onLangChange
       .subscribe((event: LangChangeEvent) => {
-        if (event.lang === 'de') {
-          this.isGerman = true;
-        } else {
-          this.isGerman = false;
-        }
+        this.setLanguage(event.lang);
       });
+  }
+
+  setLanguage(lang: string) {
+    if (lang === 'de') {
+      this.isGerman = true;
+    } else {
+      this.isGerman = false;
+    }
   }
 
   sendGAEvent(category: string, action: string, label: string, value?: number) {
