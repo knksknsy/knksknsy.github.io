9af023f379b72b1d8a4c5b75ba565c845b7e24e0
knksknsy <k.keskinsoy@gmail.com>
2019-02-06 14:33:35 +0100

    Refactor CVComponent.

--- src/app/ui/cv/cv.component.ts
+++ src/app/ui/cv/cv.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import * as jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cv',
@@ -9,65 +7,22 @@ import html2canvas from 'html2canvas';
   styleUrls: ['./cv.component.scss']
 })
 export class CVComponent implements OnInit {
+  
+  public isGerman: boolean;
+  public fileGerman = 'Lebenslauf_Kaan_Keskinsoy';
+  public fileEnglish = 'CV_Kaan_Keskinsoy';
 
-  private mimeType = 'image/svg+xml;charset=utf-8';
-  private dimension = { width: 210, height: 297 };
-  private fileName = 'Lebenslauf_Kaan_Keskinsoy.pdf';
-  private pdf: jsPDF;
-  private printSections: HTMLCollectionOf<Element>;
-  private contactIconsElement: Element;
-  private contactTextsElement: Element;
-
-  printing: boolean = false;
-
-  constructor() { }
+  constructor(public translate: TranslateService) { }
 
   ngOnInit() {
-  }
-
-  printCV() {
-    this.sendGAEvent('PDF', 'save', 'Curriculum Vitae');
-    this.printing = true;
-    this.printSections = document.getElementsByClassName('print-section');
-    let promises = [];
-    this.pdf = new jsPDF('p', 'mm', 'a4', true);
-    this.contactIconsElement = document.getElementsByClassName('contact')[0];
-    this.contactTextsElement = document.getElementsByClassName('contact-alt')[0];
-
-    for (let i = 0; i < this.printSections.length; i++) {
-      promises.push(html2canvas(this.printSections[i]));
-    }
-
-    this.hideElement(this.contactIconsElement);
-    this.showElement(this.contactTextsElement);
-
-    Promise.all(promises)
-      .then((canvases: [HTMLCanvasElement]) => {
-        canvases.map(this.appendToPDF, this);
+    this.translate.onLangChange
+      .subscribe((event: LangChangeEvent) => {
+        if (event.lang === 'de') {
+          this.isGerman = true;
+        } else {
+          this.isGerman = false;
+        }
       });
-  }
-
-  private appendToPDF(canvas: HTMLCanvasElement, index: number) {
-    const contentDataURL = canvas.toDataURL(this.mimeType);
-    this.pdf.addImage(contentDataURL, 'JPEG', 0, 0, this.dimension.width, this.dimension.height, null, 'FAST');
-    if (index < this.printSections.length - 1) {
-      this.pdf.addPage('a4', 'p');
-    } else {
-      this.pdf.save(this.fileName)
-      this.showElement(this.contactIconsElement);
-      this.hideElement(this.contactTextsElement);
-      this.printing = false;
-    }
-  }
-
-  private hideElement(element: Element): Element {
-    element.className += ' hide';
-    return element;
-  }
-
-  private showElement(element: Element): Element {
-    element.className = element.className.replace(' hide', '');
-    return element;
   }
 
   sendGAEvent(category: string, action: string, label: string, value?: number) {
